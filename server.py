e85cb0e9eeeff333369116d324b7a90f1aed8534
99Flake <36083052+KingCole01@users.noreply.github.com>
2021-02-09 16:06:10 +0000

    fixed logging error line 90

--- server.py
+++ server.py
@@ -87,7 +87,7 @@ def launch_socket():
                         progress.update(len(bytes_read))
                 finally:
                     output = pickle.loads(buffer)
-                    logger.info('SOCKET - output: ', output)
+                    logger.info('SOCKET - output: ', str(output))
                     database.writeToDatabase(output)
                     break
 
