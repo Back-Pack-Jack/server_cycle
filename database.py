fb175ecb5550aa3eb83b25cf6d2eba2184b94a88
99Flake <36083052+KingCole01@users.noreply.github.com>
2021-03-29 16:05:38 +0100

    amended sql query

--- database.py
+++ database.py
@@ -40,12 +40,10 @@ def writeToDatabase(output):
     #add try statement to flag records that are incorrect along with the device for debugging
     for _, record in enumerate(output):
         if len(record) <=4:
-            cur.execute("DATABASE - INSERT INTO detections (id, gate, datetime, classification) VALUES (%s, %s, %s, %s) ", (record[0], record[1], datetime.datetime.fromtimestamp(record[2]), record[3])) # Temp Fix
-    logger.info("DATABASE - Inserted Detections Into Database")
+            cur.execute("INSERT INTO detections (id, gate, datetime, classification) VALUES (%s, %s, %s, %s) ", (record[0], record[1], datetime.datetime.fromtimestamp(record[2]), record[3])) # Temp Fix
     conn.commit()
-    logger.info("DATABASE - Commited Detections To Database")
+    logger.info("DATABASE - Inserted Detections Into Database")
     cur.close()
-    logger.info("DATABASE - Closed Cursor")
     conn.close()
     logger.info("DATABASE - Closed Connection")
 
@@ -55,13 +53,11 @@ def addDeviceToDB(output):
     logger.info("DATABASE - Opened connection To Database")
     cur = conn.cursor() # Open a cursor to perform db operations
     try:
-        cur.execute("DATABASE - INSERT INTO devices (device_id, device_name) VALUES (%s, %s) ", (output, ''))
-        logger.info("DATABASE - Inserted new device into database")
+        cur.execute("INSERT INTO devices (device_id, device_name) VALUES (%s, %s) ", (output, ''))
         conn.commit()
-        logger.info("DATABASE - Commited Detections To Database")
+        logger.info("DATABASE - Inserted new device into database")
     except:
         logger.error("DATABASE - Unable to add the device. UUID already exists within database")
     cur.close()
-    logger.info("DATABASE - Closed Cursor")
     conn.close()
     logger.info("DATABASE - Closed Connection")
